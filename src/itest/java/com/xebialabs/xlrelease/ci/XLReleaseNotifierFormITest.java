6fa1732a67554ce572a7f8bf974a315d68f8f582
Bulat Yaminov <byaminov@xebialabs.com>
2015-03-10 14:25:54 +0100

    An integration test which starts a release; making XLR server used for itests configurable

--- src/itest/java/com/xebialabs/xlrelease/ci/XLReleaseNotifierFormITest.java
+++ src/itest/java/com/xebialabs/xlrelease/ci/XLReleaseNotifierFormITest.java
@@ -23,24 +23,43 @@
 
 package com.xebialabs.xlrelease.ci;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.recipes.LocalData;
+import org.kohsuke.stapler.StaplerRequest;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlSelect;
 
+import hudson.model.Descriptor;
+import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
+import hudson.util.Secret;
+import net.sf.json.JSONObject;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class XLReleaseNotifierFormITest {
 
     private static final String USER_VARIABLE = "${user}";
     private static final String TEMPLATE_NAME = "Welcome to XL Release!";
     private static final String RELEASE_TITLE = "Release created with jenkins plugin ${BUILD_NUMBER}";
+    private static final String ADMIN_CREDENTIAL = "admin_credential";
+
+    private static final String ENV_XLR_HOST = "xlReleaseIntegration.host";
+    private static final String ENV_XLR_USERNAME = "xlReleaseIntegration.username";
+    private static final String ENV_XLR_PASSWORD = "xlReleaseIntegration.password";
 
     @Rule
     public JenkinsRule jenkins = new JenkinsRule();
@@ -49,7 +68,8 @@ public class XLReleaseNotifierFormITest {
     @LocalData
     public void shouldShowListOfTemplatesWithSavedAsPreSelected() throws Exception {
         FreeStyleProject project = jenkins.createFreeStyleProject();
-        XLReleaseNotifier before = new XLReleaseNotifier("admin_credential", TEMPLATE_NAME, RELEASE_TITLE, null, true);
+        XLReleaseNotifier before = reconfigureWithEnvSettings(
+                new XLReleaseNotifier(ADMIN_CREDENTIAL, TEMPLATE_NAME, RELEASE_TITLE, null, true));
         project.getPublishersList().add(before);
 
         HtmlForm xlrForm = jenkins.createWebClient().getPage(project, "configure").getFormByName("config");
@@ -59,6 +79,53 @@ public class XLReleaseNotifierFormITest {
         assertThat(templateSelect.getOptionSize(), greaterThan(1));
         assertThat(xlrForm.getInputByName("_.version").asText(), equalTo(RELEASE_TITLE));
         assertThat(xlrForm.getSelectByName("_.propertyName").getSelectedOptions().get(0).asText(), equalTo(USER_VARIABLE));
+    }
+
+    @Test
+    @LocalData
+    public void shouldStartRelease() throws Exception {
+        FreeStyleProject project = jenkins.createFreeStyleProject();
+        XLReleaseNotifier before = reconfigureWithEnvSettings(new XLReleaseNotifier(
+                ADMIN_CREDENTIAL, TEMPLATE_NAME, RELEASE_TITLE,
+                newArrayList(new NameValuePair(USER_VARIABLE, "jenkins")), true));
+        project.getPublishersList().add(before);
+        FreeStyleBuild freeStyleBuild = jenkins.buildAndAssertSuccess(project);
+        String releaseId = findReleaseId(freeStyleBuild.getLog(20));
+        assertThat(releaseId, notNullValue());
+
+    }
+
+    private String findReleaseId(List<String> log) {
+        for (String line : log) {
+            Matcher matcher = Pattern.compile(".*(Release\\d+).*").matcher(line);
+            if (matcher.matches()) {
+                return matcher.group(1);
+            }
+        }
+        return null;
+    }
+
+    private XLReleaseNotifier reconfigureWithEnvSettings(XLReleaseNotifier xlReleaseNotifier) throws Descriptor.FormException {
+
+        final String host = System.getProperty(ENV_XLR_HOST);
+        if (host == null) {
+            // no custom server configured
+            return xlReleaseNotifier;
+        }
+        final String username = System.getProperty(ENV_XLR_USERNAME);
+        final String password = System.getProperty(ENV_XLR_PASSWORD);
+
+
+        StaplerRequest request = mock(StaplerRequest.class);
+        when(request.bindJSONToList(eq(Credential.class), anyObject())).thenReturn(newArrayList(
+                new Credential(ADMIN_CREDENTIAL, username, Secret.fromString(password), null)));
+        JSONObject json = new JSONObject();
+        json.put("xlReleaseServerUrl", host);
+        json.put("xlReleaseClientProxyUrl", "");
+
+        xlReleaseNotifier.getDescriptor().configure(request, json);
+
+        return xlReleaseNotifier;
     }
 
 }
