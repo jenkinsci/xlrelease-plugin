33b87fa2e1e34518644749eff66a7e89f14eede4
Wai Lee <wlee@xebialabs.com>
2019-06-05 16:25:56 -0400

    XLINT-458 Fixed non-default URL was not picked up when in use; fixed the collision of key when looking up the XLReleaseServerConnector in the HashMap

--- src/main/java/com/xebialabs/xlrelease/ci/server/XLReleaseServerConnectorFactory.java
+++ src/main/java/com/xebialabs/xlrelease/ci/server/XLReleaseServerConnectorFactory.java
@@ -1,6 +1,7 @@
 package com.xebialabs.xlrelease.ci.server;
 
 import com.xebialabs.xlrelease.ci.Credential;
+import com.xebialabs.xlrelease.ci.XLReleaseNotifier.XLReleaseDescriptor;
 
 import java.util.Map;
 import java.util.logging.Logger;
@@ -8,10 +9,10 @@ import java.util.logging.Logger;
 import static com.google.common.collect.Maps.newHashMap;
 
 public class XLReleaseServerConnectorFactory {
-    private final static Logger LOGGER = Logger.getLogger(XLReleaseServerConnectorFactory.class.getName());
     private String serverUrl;
     private String proxyUrl;
     private transient static XLReleaseServerFactory xlReleaseServerFactory = new XLReleaseServerFactory();
+    private final static Logger LOGGER = Logger.getLogger(XLReleaseServerConnectorFactory.class.getName());
 
 
     public void load(String serverUrl, String proxyUrl) {
@@ -21,18 +22,32 @@ public class XLReleaseServerConnectorFactory {
 
     public XLReleaseServerConnector getXLReleaseServerConnector(Credential credential, Map<String, XLReleaseServerConnector> credentialServerMap) {
         XLReleaseServerConnector xlReleaseServer = null;
+        
         if (null != credential) {
+        	String _serverUrl = credential.showSecondaryServerSettings()?credential.getSecondaryServerUrl():serverUrl;
+        	String _proxyUrl = credential.showSecondaryServerSettings()?credential.getSecondaryProxyUrl():proxyUrl;
+        	String credKey = credential.getKey()+":"+_serverUrl;
 
-            XLReleaseServerConnector xlReleaseServerConnectorServerRef = credentialServerMap.get(credential.getKey());
+            XLReleaseServerConnector xlReleaseServerConnectorServerRef = credentialServerMap.get(credKey);
 
             if (null != xlReleaseServerConnectorServerRef) {
                 xlReleaseServer = xlReleaseServerConnectorServerRef;
+                LOGGER.info("XLReleaseServerConnector found in the HashMap using key for username=" + credential.getUsername()+ ", server url=" + _serverUrl);
+            }
+            if(credential.showSecondaryServerSettings()) {
+            	 LOGGER.info("non-default server URL=" + credential.getSecondaryServerUrl());
+            	 LOGGER.info("non-default proxy server URL=" + credential.getSecondaryProxyUrl());
+            }
+            else {
+		            LOGGER.info("default server URL=" + serverUrl);
+		            LOGGER.info("default proxy server URL=" + proxyUrl);
             }
 
             if (null == xlReleaseServer) {
                 synchronized (this) {
-                    xlReleaseServer = xlReleaseServerFactory.newInstance(serverUrl, proxyUrl, credential);
-                    credentialServerMap.put(credential.getKey(), xlReleaseServerFactory.newInstance(serverUrl, proxyUrl, credential));
+                	LOGGER.info("XLReleaseServerConnector not found in the HashMap....create a new instance using key for username=" + credential.getUsername()+ ", server url=" + _serverUrl);
+                    xlReleaseServer = xlReleaseServerFactory.newInstance(_serverUrl, _proxyUrl, credential);
+                    credentialServerMap.put(credKey, xlReleaseServer);
                 }
             }
         }
