134273e0b7fcb41b7980785cd9fbbd4cc334b75d
Wai Lee <wlee@xebialabs.com>
2019-06-05 17:05:47 -0400

    XLINT-458 Fixed non-default URL was not picked up when in use; fixed … (#38)
    * XLINT-458 Fixed non-default URL was not picked up when in use; fixed the non-unique key issue when looking up the XLReleaseServerConnector in the HashMap
    
    * XLINT-458 Removed unused imports; added comments about the fixes.

--- src/main/java/com/xebialabs/xlrelease/ci/server/XLReleaseServerConnectorFactory.java
+++ src/main/java/com/xebialabs/xlrelease/ci/server/XLReleaseServerConnectorFactory.java
@@ -5,13 +5,11 @@ import com.xebialabs.xlrelease.ci.Credential;
 import java.util.Map;
 import java.util.logging.Logger;
 
-import static com.google.common.collect.Maps.newHashMap;
-
 public class XLReleaseServerConnectorFactory {
-    private final static Logger LOGGER = Logger.getLogger(XLReleaseServerConnectorFactory.class.getName());
     private String serverUrl;
     private String proxyUrl;
     private transient static XLReleaseServerFactory xlReleaseServerFactory = new XLReleaseServerFactory();
+    private final static Logger LOGGER = Logger.getLogger(XLReleaseServerConnectorFactory.class.getName());
 
 
     public void load(String serverUrl, String proxyUrl) {
@@ -21,18 +19,38 @@ public class XLReleaseServerConnectorFactory {
 
     public XLReleaseServerConnector getXLReleaseServerConnector(Credential credential, Map<String, XLReleaseServerConnector> credentialServerMap) {
         XLReleaseServerConnector xlReleaseServer = null;
+        
         if (null != credential) {
+        	String _serverUrl = credential.showSecondaryServerSettings()?credential.getSecondaryServerUrl():serverUrl;
+        	String _proxyUrl = credential.showSecondaryServerSettings()?credential.getSecondaryProxyUrl():proxyUrl;
+        	
+        	//XLINT-458 and 706, the key used to look up the XLReleaseServerConnector is not unique
+        	//For an example: XLR servers traditionally have username:password setup as admin:admin
+        	//the key was set as username + ":" + password.getPlainText() + "@" + name + ":" + credentialsId + ":"
+        	//will return the first stored server connector when the key is matched. Without considering the URL, it ends up job to be executed in wrong server.
+        	//added server URL as part of key to distinguish them.
+        	String credKey = credential.getKey()+":"+_serverUrl;
 
-            XLReleaseServerConnector xlReleaseServerConnectorServerRef = credentialServerMap.get(credential.getKey());
+            XLReleaseServerConnector xlReleaseServerConnectorServerRef = credentialServerMap.get(credKey);
 
             if (null != xlReleaseServerConnectorServerRef) {
                 xlReleaseServer = xlReleaseServerConnectorServerRef;
+                LOGGER.info("XLReleaseServerConnector found in the HashMap using key for username=" + credential.getUsername()+ ", server url=" + _serverUrl);
+            }
+            if(credential.showSecondaryServerSettings()) {
+            	 LOGGER.info("non-default server URL=" + credential.getSecondaryServerUrl());
+            	 LOGGER.info("non-default proxy server URL=" + credential.getSecondaryProxyUrl());
+            }
+            else {
+		            LOGGER.info("default server URL=" + serverUrl);
+		            LOGGER.info("default proxy server URL=" + proxyUrl);
             }
 
             if (null == xlReleaseServer) {
                 synchronized (this) {
-                    xlReleaseServer = xlReleaseServerFactory.newInstance(serverUrl, proxyUrl, credential);
-                    credentialServerMap.put(credential.getKey(), xlReleaseServerFactory.newInstance(serverUrl, proxyUrl, credential));
+                	LOGGER.info("XLReleaseServerConnector not found in the HashMap....create a new instance using key for username=" + credential.getUsername()+ ", server url=" + _serverUrl);
+                    xlReleaseServer = xlReleaseServerFactory.newInstance(_serverUrl, _proxyUrl, credential);
+                    credentialServerMap.put(credKey, xlReleaseServer);
                 }
             }
         }
